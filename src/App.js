1a364082acc2021ca8342d062d80c5c489f26f74
Somya <somyabakshi5@gmail.com>
2022-07-04 15:17:45 +0530

    fixed gsap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React,{useRef,useEffect} from "react";
 import Landing from "./Landing/Landing";
 import Nav from "./Nav/Nav";
 import Footer from "./Footer/Footer";
+import './App.css';
+import {gsap} from "gsap";
 
 function App() {
+  let app = useRef(null);
+  useEffect(() => {
+    gsap.to(app,0,{css: {visibility:'visible'}})
+  
+    // return () => {
+    //   second
+    // }
+  }, [])
+  
   return (
-    <>
+    <div ref={e=> app=e} className="app">
       <Nav />
       <Landing />
       <Footer />
-    </>
+    </div>
   );
 }
 
